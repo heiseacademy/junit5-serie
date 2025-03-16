43196f06bb1cf183e097a89402324ea96a7415e1
Sven Ruppert <sven.ruppert@gmail.com>
2025-03-16 10:28:08 +0100

    switching to new folder structure

---
+++ module_03/src/test/java/junit/com/svenruppert/junit5/basics/c07/C0703Test.java
@@ -0,0 +1,73 @@
+package junit.com.svenruppert.junit5.basics.c07;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+
+import static java.lang.System.out;
+import static java.lang.Thread.currentThread;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
+
+@Execution(CONCURRENT)
+public class C0703Test {
+
+  @Test
+  void test001() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 001 " + currentThread().getName());
+  }
+
+  @Test
+  void test002() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 002 " + currentThread().getName());
+  }
+
+  @Test
+  void test003() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 003 " + currentThread().getName());
+  }
+
+  @Test
+  void test004() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 004 " + currentThread().getName());
+  }
+
+  @Test
+  void test005() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 005 " + currentThread().getName());
+  }
+
+  @Test
+  void test006() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 006 " + currentThread().getName());
+  }
+
+  @Test
+  void test007() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 007 " + currentThread().getName());
+  }
+
+  @Test
+  void test008() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 008 " + currentThread().getName());
+  }
+
+  @Test
+  void test009() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 009 " + currentThread().getName());
+  }
+
+  @Test
+  void test010() throws InterruptedException {
+    SECONDS.sleep(2);
+    out.println("Running Test 010 " + currentThread().getName());
+  }
+}
---
+++ module_03/src/test/java/junit/com/svenruppert/junit5/basics/step01/DemoServiceTest.java
@@ -0,0 +1,22 @@
+package junit.com.svenruppert.junit5.basics.step01;
+
+import com.svenruppert.junit5.basics.step01.DemoService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class DemoServiceTest {
+
+
+  @Test
+  void doSomeWorkA() {
+    //here should be something
+    DemoService demoService = new DemoService();
+    String result = demoService.doSomeWork(10);
+  }
+
+  @Test
+  @Disabled
+  void test001() {
+    throw new NullPointerException("Einfach so");
+  }
+}
